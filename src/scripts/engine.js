5fa8bc08bf9a46d920af905d001e1e2b2318f3b7
MottaJr <motta.jr1981@outlook.com>
2024-11-29 04:59:28 -0300

    Projeto Finalizado
    Foi adicionado novas cartas;
    Foi criado um model para mostrar o final do jogo;
    Foi adicionado condicional para que as cartas não pudessem ser clicadas mais de 1 vez ou após o termino do jogo sem que desse um refresh
    Foi adicionado sons para deixar o jogo um pouco mais descontraido.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,4 @@
-const emojis = [
+/*const emojis = [
   "🐱",
   "🐱",
   "🦝",
@@ -15,20 +15,54 @@ const emojis = [
   "🐯",
   "🐮",
   "🐮",
+];*/
+
+const imagens = [
+  "alegria.png",
+  "alegria.png",
+  "ansiedade.png",
+  "ansiedade.png",
+  "inveja.png",
+  "inveja.png",
+  "medo.png",
+  "medo.png",
+  "nojinho.png",
+  "nojinho.png",
+  "raiva.png",
+  "raiva.png",
+  "tristeza.png",
+  "tristeza.png",
+  "vergonha.png",
+  "vergonha.png"
 ];
 let openCards = [];
 
-let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+let shuffleImagens = imagens.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for (let i = 0; i < emojis.length; i++) {
+let gameOver = false;  // Estado que indica se o jogo foi vencido ou não
+
+const modal = document.querySelector("dialog");
+
+for (let i = 0; i < imagens.length; i++) {
   let box = document.createElement("div");
   box.className = "item";
-  box.innerHTML = shuffleEmojis[i];
+  //box.innerHTML = shuffleImagens[i];
+
+  // Criar o elemento <img> e configurar a imagem
+  let img = document.createElement("img");
+  img.src = `./src/images/${shuffleImagens[i]}`; // Caminho para as imagens
+  img.alt = shuffleImagens[i]; // Usando o nome da imagem como texto alternativo
+
+  box.appendChild(img);
   box.onclick = handleClick;
   document.querySelector(".game").appendChild(box);
 }
 
 function handleClick() {
+  /* Aqui foi criado uma condição para que após a vitória ou um item 
+  já já tiver sido clicado, impedir qualquer interção*/
+  if (gameOver || this.classList.contains("boxMatch") || this.classList.contains("boxOpen")) return;
+
   if (openCards.length < 2) {
     this.classList.add("boxOpen");
     openCards.push(this);
@@ -41,18 +75,37 @@ function handleClick() {
   console.log(openCards);
 }
 
+function playSound(audioName) { //adiciona audios no jogo
+  let audio = new Audio(`./src/audios/${audioName}.mp3`);
+  audio.volume = 0.3;
+  audio.play();
+}
+
+function abrirModal() {
+  modal.show()
+}
+
 function checkMatch() {
   if (openCards[0].innerHTML === openCards[1].innerHTML) {
     openCards[0].classList.add("boxMatch");
     openCards[1].classList.add("boxMatch");
+
+    playSound("acerto"); //audio de acerto
   } else {
+    playSound("errou"); //audio de erro
     openCards[0].classList.remove("boxOpen");
     openCards[1].classList.remove("boxOpen");
   }
 
   openCards = [];
 
-  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
-    alert("Você venceu !");
+  if (document.querySelectorAll(".boxMatch").length === imagens.length) {
+
+    playSound("vitoria"); //audio de vitoria
+
+    //Abre modal de vitória
+    abrirModal();
+
+    if (gameOver) return; //informa que o jogo foi vencido
   }
 }
